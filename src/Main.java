1f9e5374289189b987c75ded3c0e7aa19c4b331e
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-19 12:03:33 -0400

    Finished all sections

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        final double TAX_PERCENTAGE = 0.05;
+        double purchasePrice = 15.00;
+        double salesTaxCost = 0.00;
 
+        salesTaxCost = purchasePrice * TAX_PERCENTAGE;
 
-
+        System.out.println("The price of your purchase is $" + purchasePrice + ".");
+        System.out.println("Your calculated sales tax is $" + salesTaxCost + ".");
     }
 }
